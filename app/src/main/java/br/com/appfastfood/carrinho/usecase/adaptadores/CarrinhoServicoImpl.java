8ba6534721dd10ba9935f276f11c5c995c3e6f73
Pedro Ramalho <pedro.ramalho@sysmap.com.br>
2024-03-02 18:40:51 -0300

    Removendo logicas negociais

--- app/src/main/java/br/com/appfastfood/carrinho/usecase/adaptadores/CarrinhoServicoImpl.java
+++ app/src/main/java/br/com/appfastfood/carrinho/usecase/adaptadores/CarrinhoServicoImpl.java
@@ -1,48 +1,30 @@
 package br.com.appfastfood.carrinho.usecase.adaptadores;
 
 import br.com.appfastfood.carrinho.aplicacao.adaptadores.requisicao.CarrinhoRequisicao;
-import br.com.appfastfood.carrinho.aplicacao.adaptadores.requisicao.ProdutosReq;
 import br.com.appfastfood.carrinho.dominio.modelos.Carrinho;
 import br.com.appfastfood.carrinho.dominio.modelos.Cliente;
 import br.com.appfastfood.carrinho.dominio.modelos.enums.StatusCarrinhoEnum;
-import br.com.appfastfood.carrinho.exceptions.ExceptionsMessages;
 import br.com.appfastfood.carrinho.infraestrutura.menssagem.CarrinhoFila;
 import br.com.appfastfood.carrinho.infraestrutura.menssagem.adaptadores.saida.CarrinhoOUT;
 import br.com.appfastfood.carrinho.usecase.portas.CarrinhoServico;
-import br.com.appfastfood.configuracoes.execption.BadRequestException;
 import br.com.appfastfood.pedido.dominio.modelos.VO.ProdutoVO;
 import br.com.appfastfood.produto.dominio.modelos.Produto;
-import br.com.appfastfood.produto.usecase.portas.ProdutoServico;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class CarrinhoServicoImpl implements CarrinhoServico {
 
-    private final ProdutoServico produtoServico;
     private CarrinhoFila carrinhoFila;
 
-    public CarrinhoServicoImpl(ProdutoServico produtoServico, CarrinhoFila carrinhoFila) {
-        this.produtoServico = produtoServico;
+    public CarrinhoServicoImpl(CarrinhoFila carrinhoFila) {
         this.carrinhoFila = carrinhoFila;
     }
 
     @Override
     public void cadastrar(CarrinhoRequisicao carrinhoRequisicao) {
-        Produto produtoBuscaId = null;
         Double valorTotal = 0.0;
         List<ProdutoVO> produtosVO = new ArrayList<>();
-
-        if(carrinhoRequisicao.getProdutos().isEmpty()) {
-            throw new BadRequestException(ExceptionsMessages.CAMPO_PRODUTO_OBRIGATORIO.getValue());
-        }
-
-        for(ProdutosReq produtoReq : carrinhoRequisicao.getProdutos()){
-            ProdutoVO produtoVO = new ProdutoVO(produtoReq.getIdProduto(), produtoReq.getQuantidadeProduto());
-            produtoBuscaId = produtoServico.buscaProdutoPorId(Long.valueOf(produtoReq.getIdProduto()));
-            valorTotal += produtoBuscaId.getPreco().getPreco() * Double.parseDouble(produtoReq.getQuantidadeProduto());
-            produtosVO.add(produtoVO);
-        }
 
         Carrinho carrinho = new Carrinho(produtosVO, new Cliente(carrinhoRequisicao.getIdCliente()), valorTotal, StatusCarrinhoEnum.ABERTO);
 
@@ -70,22 +52,8 @@ public class CarrinhoServicoImpl implements CarrinhoServico {
 
     @Override
     public Carrinho atualizar(Long id, CarrinhoRequisicao carrinhoRequisicao) {
-        Produto produtoBuscaId = null;
         Double valorTotal = 0.0;
-
         List<ProdutoVO> produtosVO = new ArrayList();
-
-        if(carrinhoRequisicao.getProdutos().isEmpty()) {
-            throw new BadRequestException(ExceptionsMessages.CAMPO_PRODUTO_OBRIGATORIO.getValue());
-        }
-
-        for(ProdutosReq produtoReq : carrinhoRequisicao.getProdutos()){
-            ProdutoVO produtoVO = new ProdutoVO(produtoReq.getIdProduto(), produtoReq.getQuantidadeProduto());
-            produtoBuscaId = produtoServico.buscaProdutoPorId(Long.valueOf(produtoReq.getIdProduto()));
-            Double quantidade = Double.parseDouble(produtoReq.getQuantidadeProduto());
-            valorTotal += produtoBuscaId.getPreco().getPreco() * quantidade;
-            produtosVO.add(produtoVO);
-        }
 
         Carrinho carrinho = new Carrinho(produtosVO, new Cliente(carrinhoRequisicao.getIdCliente()), valorTotal);
 
